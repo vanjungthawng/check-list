188e67ba052ad7c106c3188cee1f50897060de08
Van Cung Thawng <vanjungthawng@gmail.com>
2022-02-08 23:32:21 +1100

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/moviesCheckList",
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
